01524b13c6d938d7c635111c721842b5857fa0c8
Administrator <Administrator@PC265>
2019-11-28 14:18:09 +0700

    Add project files.

---
+++ bai1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap tong so sinh vien N =");
+            int n = int.Parse(Console.ReadLine());
+           
+
+            List<SinhVien> listStudent = new List<SinhVien>();
+            Console.WriteLine("\n ===NHAP DS SINH VIEN====");
+            for (int i = 0; i < n; i++)
+            {
+                SinhVien temp = new SinhVien();
+                temp.Input();
+                listStudent.Add(temp);
+            }
+
+            Console.WriteLine("\n ===XUAT DS SINH VIEN===");
+            foreach (SinhVien sv in listStudent)
+            {
+                sv.Show();
+            }
+        }
+    }
+}
---
+++ bai1/SinhVien.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    class SinhVien
+    {
+        private string studentID;
+        private string fullName;
+        private float averageScore;
+        private string faculty;
+
+        public string StudentID { get; set; }
+        public string FullName { get; set; }
+        public float AverageScor { get; set; }
+        public string Faculty { get; set; }
+
+        public SinhVien()
+        {
+
+        }
+
+        public SinhVien(string id, string name, float score, string faculty)
+        {
+            studentID = id;
+            fullName = name;
+            averageScore = score;
+            faculty = faculty; 
+        }
+        
+        public void Input()
+        {
+            Console.Write("Nhap MSSV: ");
+            StudentID = Console.ReadLine();
+
+            Console.Write("Nhap Diem: ");
+            AverageScor = float.Parse(Console.ReadLine());
+
+        }
+        public void Show()
+        {
+            Console.WriteLine("MSSV: {0} Ho Ten:{1} Khoa:{2} DiemTB:{3}", this.StudentID, this.FullName, this.Faculty, this.AverageScor);
+        }
+            
+        
+    }
+
+
+}
